5174aafb8ecf98f064017cfd6159322bbba12b38
neukkken <santyagonarvaezns@gmail.com>
2026-02-03 17:31:15 -0500

    hotfix: background moon img object-contain added

--- src/components/background-moon.tsx
+++ src/components/background-moon.tsx
@@ -4,7 +4,7 @@ export default function BackgroundMoon() {
     return (
         <div className='absolute top-0 left-0 w-full h-full -z-10'>
             <div className="background-container">
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className='imgMoon'/>
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png" alt="moon" className='imgMoon object-contain'/>
                 <div className="stars"></div>
                 <div className="twinkling"></div>
                 <div className="clouds"></div>
